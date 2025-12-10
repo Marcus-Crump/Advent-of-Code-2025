20e1e4dae78aa04ec4d6cd66cc742b2a6044b32e
Marcus-Crump <mlc5638@eid.utexas.edu>
2025-12-10 23:01:26 +0900

    Here's my solution to day 2 with all its test cases as well. At this pace, we'll see if I even finish them all before christmas

--- Day 2/Day_2_Gift_Shop.py
+++ Day 2/Day_2_Gift_Shop.py
@@ -4,7 +4,35 @@ def is_invalid_p1(val:int) -> bool:
     invalid = val[:l] == val[l:]
     return invalid
 
-def main_p1(ids:list)-> int:
+def is_invalid_pt2(val:int) ->bool:
+    if is_invalid_p1(val): return True
+
+    val = str(val)
+
+    for end in range(len(val)//2):
+        sample = val[:end+1]
+        skip = len(sample)
+        valid = True
+        for i in range(end+1, len(val),skip):
+            comp = val[i:i+skip]
+            if sample != comp:
+                valid = False
+                break
+        if valid: return True
+        
+    return False
+
+def main(ids:list)-> int:
+    sum = 0
+    for id in ids:
+        l,h = id.split(sep="-")
+        for i in range(int(l),int(h)+1):
+            if is_invalid_pt2(i):
+                sum += i
+    
+    return sum
+
+def day_1(ids:list)->int:
     sum = 0
     for id in ids:
         l,h = id.split(sep="-")
@@ -14,10 +42,7 @@ def main_p1(ids:list)-> int:
     
     return sum
 
-def main_p2(ids:list)-> int:
-    pass
-
 if __name__ == "__main__":
     with open("Day_2_Input.txt") as file:
-        out = main_p1(file.readlines()[0].split(sep=','))
+        out = main(file.readlines()[0].split(sep=','))
         print(out)
--- Day 2/Day_2_Tests.py
+++ Day 2/Day_2_Tests.py
@@ -5,13 +5,60 @@ class TestCases(unittest.TestCase):
     def test_1(self):
         with open("Day_2_Sample.txt", 'r') as file:
             expected = 1227775554
-            actual = d2.main(file.readlines()[0].split(sep=","))
+            actual = d2.day_1(file.readlines()[0].split(sep=","))
             
             try:
                 self.assertEqual(actual,expected)
                 print("PASSED 1")
             except AssertionError:
                 print("Testcase 1 failed")
+                print(f"Actual: {actual} != Expected:{expected}")
+    def test_sliding_0(self):
+        num = 111111111
+        try:
+            self.assertTrue(d2.is_invalid_pt2(num))
+            print("PASSED SLIDE TEST 0")
+        except AssertionError:
+            print("Slide test 0 did not work")
+        
+    def test_sliding_1(self):
+        num = 12121212
+        try:
+            self.assertTrue(d2.is_invalid_pt2(num))
+            print("PASSED SLIDE TEST 1")
+        except AssertionError:
+            print("Slide test 1 did not work")
+    def test_sliding_2(self):
+        num = 123123123
+        try:
+            self.assertTrue(d2.is_invalid_pt2(num))
+            print("PASSED SLIDE TEST 2")
+        except AssertionError:
+            print("Slide test 2 did not work")
+    def test_sliding_3(self):
+        num = 12341234
+        try:
+            self.assertTrue(d2.is_invalid_pt2(num))
+            print("PASSED SLIDE TEST 3")
+        except AssertionError:
+            print("Slide test 3 did not work")
+    def test_sliding_4(self):
+        num = 1234512345
+        try:
+            self.assertTrue(d2.is_invalid_pt2(num))
+            print("PASSED SLIDE TEST 4")
+        except AssertionError:
+            print("Slide test 4 did not work")
+    def test_part_2(self):
+        with open("Day_2_Sample.txt", 'r') as file:
+            expected = 4174379265
+            actual = d2.main(file.readlines()[0].split(sep=","))
+            
+            try:
+                self.assertEqual(actual,expected)
+                print("PASSED DAY 2 SAMPLE")
+            except AssertionError:
+                print("Day 2 testcase 1 failed")
                 print(f"Actual: {actual} != Expected:{expected}")
 
 if __name__ == "__main__":
